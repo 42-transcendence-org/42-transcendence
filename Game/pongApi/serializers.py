fe5e08ba314967de9fa2667a84a5e1a2691bd83a
victor <victor.burton@hotmail.fr>
2023-12-05 16:55:26 +0100

    first micro-service

--- Game/pongApi/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Game
-
-class GameSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Game
-        fields = ['player1', 'player2', 'score']
--- Game/pongApi/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from rest_framework import viewsets
-from .models import Game
-from .serializers import GameSerializer
-
-class GameViewSet(viewsets.ModelViewSet):
-    queryset = Game.objects.all()
-    serializer_class = GameSerializer
