64c5ffbf08e49edc807208c0aa808fe63400c654
Victor <victor.burton@hotmail.fr>
2024-01-04 14:34:55 +0000

    a little clean in front jsScript

--- PONG/frontend/src/frontend/static/js/authScript.js
+++ PONG/frontend/src/frontend/static/js/authScript.js
@@ -7,68 +7,74 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
     authFormContainer.addEventListener('submit', function(event) {
+		event.preventDefault(); // Prevent the default form submission
         if (event.target.id === 'registerForm') {
-            event.preventDefault(); // Prevent the default form submission
-
-            const formData = new FormData(event.target); // Use the event target which is the form
-            const username = formData.get('username');
-            const password1 = formData.get('password1');
-            const password2 = formData.get('password2');
-
-            fetch('http://localhost:8002/register/', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'X-CSRFToken': csrftoken,
-                },
-                body: JSON.stringify({ username, password1, password2 })
-            })
-            .then(response => response.text())
-            .then(html => {
-                authFormContainer.innerHTML = html;
-                document.getElementById('authFormContainer').innerHTML = html;
-            })
-            .catch(error => {
-                console.error('Error:', error);
-            });
+			sendRegisterForm(csrftoken);
         }
 
         if (event.target.id === 'loginForm') {
-            event.preventDefault(); // Prevent the default form submission
-
-            const formData = new FormData(event.target); // Use the event target which is the form
-            const username = formData.get('username');
-            const password = formData.get('password');
-
-            fetch('http://localhost:8002/login/', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'X-CSRFToken': csrftoken,
-                },
-                body: JSON.stringify({ username, password }),
-                credentials: 'include'
-            })
-            .then(response => response.text())
-            .then(html => {
-				if (html.includes('Indentifiant ou mot de passe incorrect.')){
-					authFormContainer.innerHTML = html;
-				}
-				else {
-					localStorage.setItem('csrftoken', csrftoken);
-                	statusUser.innerHTML = html;
-                	document.getElementById('statusUser').innerHTML = html;
-                	authFormContainer.innerHTML = ''
-				}
-            })
-            .catch(error => {
-				console.error('Error:', error);
-            });
+			sendLoginForm(csrftoken);
         }
 
 
     });
 });
+
+function sendLoginForm(csrftoken){
+
+	const formData = new FormData(event.target); // Use the event target which is the form
+	const username = formData.get('username');
+	const password = formData.get('password');
+
+	fetch('http://localhost:8002/login/', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			'X-CSRFToken': csrftoken,
+		},
+		body: JSON.stringify({ username, password }),
+		credentials: 'include'
+	})
+	.then(response => response.text())
+	.then(html => {
+		if (html.includes('Indentifiant ou mot de passe incorrect.')){
+			authFormContainer.innerHTML = html;
+		}
+		else {
+			localStorage.setItem('csrftoken', csrftoken);
+			statusUser.innerHTML = html;
+			document.getElementById('statusUser').innerHTML = html;
+			authFormContainer.innerHTML = ''
+		}
+	})
+	.catch(error => {
+		console.error('Error:', error);
+	});
+}
+
+function sendRegisterForm(csrftoken){
+	const formData = new FormData(event.target); // Use the event target which is the form
+	const username = formData.get('username');
+	const password1 = formData.get('password1');
+	const password2 = formData.get('password2');
+
+	fetch('http://localhost:8002/register/', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			'X-CSRFToken': csrftoken,
+		},
+		body: JSON.stringify({ username, password1, password2 })
+	})
+	.then(response => response.text())
+	.then(html => {
+		authFormContainer.innerHTML = html;
+		document.getElementById('authFormContainer').innerHTML = html;
+	})
+	.catch(error => {
+		console.error('Error:', error);
+	});
+}
 
 function getCookie(name) {
     let cookieValue = null;
--- PONG/frontend/src/frontend/static/js/diplayAuth.js
+++ PONG/frontend/src/frontend/static/js/diplayAuth.js
@@ -1,18 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const RegisterForm = document.getElementById('RegisterForm');
-    const LoginForm = document.getElementById('LoginForm');
     const authFormContainer = document.getElementById('authFormContainer');
     const statusUser = document.getElementById('statusUser');
 	const csrftoken = localStorage.getItem('csrftoken');
-	console.log(csrftoken);
 
     let isRegisterForm = false;
     let isLoginForm = false;
 
-	// let check = isLoggedIn();
-	// console.log('check = ', check);
+    firstView();
 
-    isLoggedIn().then(isAuthenticated => {
+	viewToDisplay(csrftoken);
+
+});
+
+function firstView(){
+	isLoggedIn().then(isAuthenticated => {
 		if (isAuthenticated) {
 			console.log('User is logged in');
 			displayLoggedInUser();
@@ -21,67 +22,80 @@ document.addEventListener('DOMContentLoaded', function() {
 			updateUIForLoggedOutUser();
 		}
 	});
+}
 
-		console.log('hello');
-        statusUser.addEventListener('click', function() {
-            const targetId = event.target.id;
+function viewToDisplay(csrftoken){
+	statusUser.addEventListener('click', function() {
+        const targetId = event.target.id;
 
 
-            if (targetId == 'RegisterForm' && !isRegisterForm){
-                fetch('http://localhost:8002/register/')  // Update this URL to your actual authentication service URL
-                .then(response => response.text())
-                .then(html => {
-                    authFormContainer.innerHTML = html;
-                    document.getElementById('authFormContainer').innerHTML = html;
-                    isFormVisible = true;
-                })
-                .catch(error => {
-                    console.error('Error fetching auth form:', error);
-                });
-                isRegisterForm = true;
-                isLoginForm = false;
-            } else if (targetId == 'LoginForm' && !isLoginForm){
-                fetch('http://localhost:8002/login/')  // Update this URL to your actual authentication service URL
-                .then(response => response.text())
-                .then(html => {
-					authFormContainer.innerHTML = html;
-                    document.getElementById('authFormContainer').innerHTML = html;
-                    isFormVisible = true;
-                })
-                .catch(error => {
-                    console.error('Error fetching auth form:', error);
-                });
-                isLoginForm = true;
-                isRegisterForm = false;
-            } else if (targetId == 'logout'){
-				fetch('http://localhost:8002/logout/', {
-					method: 'POST',
-					credentials: 'include',
-					headers: {
-						'Content-Type': 'application/json',
-						'X-CSRFToken': csrftoken,
-					}
-				})
-				.then(response => {
-					if (response.ok) {
-						localStorage.removeItem('authToken')
-						deleteCookie('sessionid');
-						updateUIForLoggedOutUser(csrftoken);
-					}
-					console.log(response.ok)
-				})
-                .catch(error => {
-                    console.error('Logout failed:', error);
-                });
-                isLoginForm = false;
-                isRegisterForm = false;
-            } else {
-                authFormContainer.innerHTML = '';
-                isLoginForm = false;
-            }
-        });
+        if (targetId == 'RegisterForm' && !isRegisterForm){
+			displayRegister();
+            isRegisterForm = true;
+            isLoginForm = false;
+        } else if (targetId == 'LoginForm' && !isLoginForm){
+			displayLogin();
+            isLoginForm = true;
+            isRegisterForm = false;
+        } else if (targetId == 'logout'){
+			displayLogout(csrftoken);
+            isLoginForm = false;
+            isRegisterForm = false;
+        } else {
+            authFormContainer.innerHTML = '';
+            isLoginForm = false;
+			isRegisterForm = false;
+        }
+    });
+}
 
-});
+function displayLogout(csrftoken){
+	fetch('http://localhost:8002/logout/', {
+		method: 'POST',
+		credentials: 'include',
+		headers: {
+			'Content-Type': 'application/json',
+			'X-CSRFToken': csrftoken,
+		}
+	})
+	.then(response => {
+		if (response.ok) {
+			localStorage.removeItem('authToken')
+			deleteCookie('sessionid');
+			updateUIForLoggedOutUser(csrftoken);
+		}
+		console.log(response.ok)
+	})
+	.catch(error => {
+		console.error('Logout failed:', error);
+	});
+}
+
+function displayLogin(){
+	fetch('http://localhost:8002/login/')  // Update this URL to your actual authentication service URL
+	.then(response => response.text())
+	.then(html => {
+		authFormContainer.innerHTML = html;
+		document.getElementById('authFormContainer').innerHTML = html;
+		isFormVisible = true;
+	})
+	.catch(error => {
+		console.error('Error fetching auth form:', error);
+	});
+}
+
+function displayRegister(){
+	fetch('http://localhost:8002/register/')  // Update this URL to your actual authentication service URL
+	.then(response => response.text())
+	.then(html => {
+		authFormContainer.innerHTML = html;
+		document.getElementById('authFormContainer').innerHTML = html;
+		isFormVisible = true;
+	})
+	.catch(error => {
+		console.error('Error fetching auth form:', error);
+	});
+}
 
 function isLoggedIn() {
     return fetch('http://localhost:8002/check-authentication/', {
