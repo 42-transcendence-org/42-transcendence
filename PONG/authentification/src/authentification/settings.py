8e9c030cdff4ae7f9db8fc45941e2593ebb1b480
Victor <victor.burton@hotmail.fr>
2024-01-05 14:48:50 +0000

    deported db

--- PONG/authentification/src/authentification/settings.py
+++ PONG/authentification/src/authentification/settings.py
@@ -93,23 +93,23 @@ WSGI_APPLICATION = 'authentification.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'authentification_db',
-#         'USER': 'postgres',
-#         'PASSWORD': 'yourpassword',
-#         'HOST': 'authentification_db',  # Name of the database service in docker-compose
-#         'PORT': '5432',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'authentification_db',
+        'USER': 'postgres',
+        'PASSWORD': 'yourpassword',
+        'HOST': 'authentification_db',  # Name of the database service in docker-compose
+        'PORT': '5432',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
