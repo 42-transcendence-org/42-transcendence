ff70392e0c90ccab41cdcf3c19fb83a5d5c648e8
victor <victor@MacBook-Pro-de-victor-2.local>
2024-03-04 14:59:13 +0100

    update ui wether user is logged or not, but right branch is testDisplayLogin refactor with matthieu

--- Micro-Services/pong_project/pong_project/static/js/authentification/diplayAuth.js
+++ Micro-Services/pong_project/pong_project/static/js/authentification/diplayAuth.js
@@ -1,115 +1,3 @@
-// import { get_cookie, div_handler } from '../utils.js';
-
-// let isLoggedIn = localStorage.getItem('isLogged');
-// let isFormVisible = false;
-// let isRegisterForm = false;
-// let isLoginForm = false;
-// document.addEventListener('DOMContentLoaded', function() {
-// 	const csrftoken = localStorage.getItem('csrftoken');
-
-//     firstView();
-// 	statusUser.addEventListener('click', function() {
-// 		const targetId = event.target.id;
-// 		viewToDisplay(targetId);
-// 	});
-
-// });
-
-// function firstView(){
-// 	if (isLoggedIn) {
-// 		console.log('User is logged');
-// 		displayLoggedInUser();
-// 	} else {
-// 		console.log('User is not logged');
-// 		updateUIForLoggedOutUser();
-// 	}
-// }
-
-// function viewToDisplay(targetId){
-
-//     if (targetId == 'RegisterForm' && !isRegisterForm){
-// 		displayRegister();
-//         isRegisterForm = true;
-//         isLoginForm = false;
-//     } else if (targetId == 'LoginForm' && !isLoginForm){
-// 		displayLogin();
-//         isLoginForm = true;
-//         isRegisterForm = false;
-//     } else if (targetId == 'logout'){
-// 		displayLogout();
-//            isLoginForm = false;
-//            isRegisterForm = false;
-//     } else {
-// 		console.log('I pass here')
-//         authFormContainer.innerHTML = '';
-//         isLoginForm = false;
-// 		isRegisterForm = false;
-//     }
-// 	console.log('lets go');
-// }
-
-// function updateUIForLoggedOutUser() {
-// 	return fetch('https://localhost:8443/auth/buttons/')  // Update this URL to your actual authentication service URL
-// 		.then(response => response.text())
-//         .then(html => {
-//             statusUser.innerHTML = html;
-//             document.getElementById('statusUser').innerHTML = html;
-//             isFormVisible = false;
-//         })
-//         .catch(error => {
-//             console.error('Error fetching auth form:', error);
-//         });
-// }
-
-// function displayLogout(){
-// 	console.log("check:", localStorage.getItem('csrftoken'));
-// 	fetch('https://localhost:8443/auth/logout/', {
-// 		method: 'POST',
-// 		credentials: 'include',
-// 		headers: {
-// 			'Content-Type': 'application/json',
-// 			'X-CSRFToken': localStorage.getItem('csrftoken'),
-// 		}
-// 	})
-// 	.then(response => {
-// 		if (response.ok) {
-// 			localStorage.removeItem('authToken')
-// 			deleteCookie('sessionid');
-// 			updateUIForLoggedOutUser();
-// 		}
-// 		console.log(response.ok)
-// 	})
-// 	.catch(error => {
-// 		console.error('Logout failed:', error);
-// 	});
-// }
-
-// function displayLogin(){
-// 	fetch('https://localhost:8443/auth/login/')
-// 	.then(response => response.text())
-// 	.then(html => {
-// 		authFormContainer.innerHTML = html;
-// 		document.getElementById('authFormContainer').innerHTML = html;
-// 		isFormVisible = true;
-// 	})
-// 	.catch(error => {
-// 		console.error('Error fetching auth form:', error);
-// 	});
-// }
-
-// function displayRegister(){
-// 	fetch('https://localhost:8443/auth/register/')
-// 	.then(response => response.text())
-// 	.then(html => {
-// 		authFormContainer.innerHTML = html;
-// 		document.getElementById('authFormContainer').innerHTML = html;
-// 		isFormVisible = true;
-// 	})
-// 	.catch(error => {
-// 		console.error('Error fetching auth form:', error);
-// 	});
-// }
-
 let isRegisterForm = false;
 let isLoginForm = false;
 document.addEventListener('DOMContentLoaded', function() {
@@ -170,7 +58,8 @@ function displayLogout(){
 	})
 	.then(response => {
 		if (response.ok) {
-			localStorage.removeItem('authToken')
+			localStorage.removeItem('authToken');
+			localStorage.removeItem('username');
 			deleteCookie('sessionid');
 			updateUIForLoggedOutUser();
 		}
@@ -187,7 +76,6 @@ function displayLogin(){
 	.then(html => {
 		authFormContainer.innerHTML = html;
 		document.getElementById('authFormContainer').innerHTML = html;
-		// isFormVisible = true;
 	})
 	.catch(error => {
 		console.error('Error fetching auth form:', error);
@@ -200,7 +88,6 @@ function displayRegister(){
 	.then(html => {
 		authFormContainer.innerHTML = html;
 		document.getElementById('authFormContainer').innerHTML = html;
-		// isFormVisible = true;
 	})
 	.catch(error => {
 		console.error('Error fetching auth form:', error);
@@ -213,8 +100,13 @@ function isLoggedIn() {
     })
         .then(response => response.json())
         .then(data => {
+			if (data.isAuthenticated){
                 console.log('Authentication check', data.isAuthenticated);
 				return true;
+			} else {
+				return false;
+			}
+			
 		})
 		.catch(error => {
 			console.error('Error checking authentication:', error);
@@ -236,7 +128,6 @@ function updateUIForLoggedOutUser() {
         .then(html => {
             statusUser.innerHTML = html;
             document.getElementById('statusUser').innerHTML = html;
-            // isFormVisible = false;
         })
         .catch(error => {
             console.error('Error fetching auth form:', error);
