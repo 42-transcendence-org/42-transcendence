f9b535571f663840a0bdcc9386703a5baa8a3446
V-Burton97 <victor.burton@hotmail.fr>
2024-03-14 08:50:28 +0100

    suppress unused comment in auth.urls

--- Micro-Services/authentification/auth_app/urls.py
+++ Micro-Services/authentification/auth_app/urls.py
@@ -4,18 +4,7 @@ from .views import LoginAPIView, RegisterAPIView, LogoutAPIView
 from . import views  # Adjust the import based on your view function or class
 
 urlpatterns = [
-
-	# #Loading html
-    # path('register_form/', views.register_form, name='register_form'),
-    # path('login_form/', views.login_form, name='login_form'),
-    # path('buttons/', views.login_buttons, name='buttons'),
-
-	# #Send Form
-    # path('register/', views.register, name='register'),
-    # path('login/', views.login_user, name='login'),
-    # path('logout/', views.logout_user, name='logout'),
-    # path('home/', views.home, name='home'),
-
+    
     path('login/', LoginAPIView.as_view(), name='api_login'),
     path('register/', RegisterAPIView.as_view(), name='api_register'),
     path('logout/', LogoutAPIView.as_view(), name='api_register'),
