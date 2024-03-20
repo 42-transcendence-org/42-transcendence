d59e39ccf0576bf2f8d9f3619db2d0a90478ec85
willrsq1 <william.ruetsuquet@gmail.com>
2024-03-20 21:34:50 +0100

    a

--- Micro-Services/authentification/auth_app/models.py
+++ Micro-Services/authentification/auth_app/models.py
@@ -24,7 +24,7 @@ class Profile(models.Model):
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
-        Profile.objects.create(user=instance)
+        Profile.objects.create(user=instance, nickname = "No Nick" + str(instance.id))
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
