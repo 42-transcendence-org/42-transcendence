da846c89de50f8262a3aa2e4096b867365432192
Victor Burton <vburton@z4r8p3.42lyon.fr>
2024-04-19 17:17:08 +0200

    work on store tournament on bc

--- Micro-Services/pong_project/pong_project/static/js/tournament.js
+++ Micro-Services/pong_project/pong_project/static/js/tournament.js
@@ -24,12 +24,34 @@ export class Tournament {
 			tourney_id: tourney_id
 		}
 		const response = await Oauth.poster(url, data);
+		const getPlayer = await Oauth.getter('/game/tournament/');
+		const check = getPlayer.history.length - 1;
+
 		if (response.error) {
 			alert(response.error);
 			return ;
 		}
+		const dataFinal = {
+			player1: getPlayer.history[check].player1,
+			player2: getPlayer.history[check].player2,
+			player3: getPlayer.history[check].player3,
+			player4: getPlayer.history[check].player4,
+			winner: getPlayer.history[check].winner,
+			game1_winner: response.games[0].winnerGame1,
+			game1_loser: response.games[0].loserGame1,
+			game2_winner: response.games[0].winnerGame2,
+			game2_loser: response.games[0].loserGame2,
+			game3_winner: response.games[0].winnerGame3,
+			game3_loser: response.games[0].loserGame3,
+			game1_player1_score: response.games[0].game1_player1_score,
+			game1_player2_score: response.games[0].game1_player2_score,
+			game2_player1_score: response.games[0].game2_player1_score,
+			game2_player2_score: response.games[0].game2_player2_score,
+			game3_player1_score: response.games[0].game3_player1_score,
+			game3_player2_score: response.games[0].game3_player2_score,
+		};
 		const urlBlockchain = '/tournament/saveTournament/';
-		const responseBlock = await Oauth.poster(urlBlockchain, response.games[0]);
+		const responseBlock = await Oauth.poster(urlBlockchain, dataFinal);
 		if (responseBlock.error) {
 			alert(responseBlock.error);
 			return ;
--- Micro-Services/tournament/tournament/blockchain/tournament.sol
+++ Micro-Services/tournament/tournament/blockchain/tournament.sol
@@ -3,43 +3,21 @@ pragma solidity ^0.8.19;
 
 contract DBTournament {
     address private owner;
-    mapping(uint64 => bool) private tournamentExist;
-    mapping(uint64 => Match[3]) private tournaments;
-
-    struct Match{
-        string score;
-        string winner;
-        string looser;
-    }
 
     constructor() {
         owner = msg.sender;
     }
 
-    function saveTournament(uint64 tournamentId, string[3] calldata _scores, string[3] calldata _winners, string[3] calldata _loosers) public onlyOwner {
-        require(!tournamentExist[tournamentId], "This tournament already exists");
-        
-        for (uint8 i = 0; i < 3; i++) {
-            Match memory newMatch = Match({
-                score: _scores[i],
-                winner: _winners[i],
-                looser: _loosers[i]
-            });
-            tournaments[tournamentId][i] = newMatch;
-        }
-        
-        tournamentExist[tournamentId] = true;
+    event tournamentSaved(string indexed tournamentOwner, string[4] players, string[3] Match1, string[3] Match2, string[3] Finale);
+
+
+    function saveTournament(string calldata _tournamentOwner, string[4] calldata _players, string[3] calldata _Match1, string[3] calldata _Match2, string[3] calldata _Finale) public onlyOwner {
+        emit tournamentSaved(_tournamentOwner, _players, _Match1, _Match2, _Finale);
     }
-
-
 
     modifier onlyOwner() {
         require(msg.sender == owner, "Only the owner can perform this action.");
         _;
     }
 
-    function getTournament(uint64 tournamentId) public view returns(Match memory, Match memory, Match memory) {
-        require(tournamentExist[tournamentId] == true, "This tournament doesn't exist");
-        return(tournaments[tournamentId][0], tournaments[tournamentId][1], tournaments[tournamentId][2]);
-    }
 }
