21d0a0bc7e3d4d6ee0834323b1e105f25e203343
Arbesa <abdyli.a@hotmail.com>
2024-04-20 12:11:47 +0200

    .

--- Micro-Services/authentification/script.sh
+++ Micro-Services/authentification/script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-(sleep 5 && python manage.py migrate) &
+(sleep 15 && python manage.py migrate) &
 uvicorn authentification.asgi:application --host 0.0.0.0 --port 8000
--- Micro-Services/game_app/script.sh
+++ Micro-Services/game_app/script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-(sleep 5 && python manage.py migrate) &
+(sleep 15 && python manage.py migrate) &
 uvicorn game_app.asgi:application --host 0.0.0.0 --port 8000
